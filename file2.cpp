4eb6f5f3c652a58c588e8b508f7a17d1b4a50191
keishashendy <shendyarrafli7@gmail.com>
2025-03-18 12:21:30 +0700

    Menambahkan perulangan while

--- file2.cpp
+++ file2.cpp
@@ -7,4 +7,19 @@ int main(){
     int x;
     int perulanganWhile = 0;
     int perulanganDoWhile = 0;
+     // perulangan while 
+     cout << "perulangan dengan while" << endl;
+     srand(time(0));
+     x = rand() % 10;
+     cout << " Nilai awal x = " << x << endl;
+     while(x < 7){
+        cout << "nilai x = " << x << " Lari keliling lapangan!" <<endl;
+        x = rand() % 10;
+        perulanganWhile++;
+     }
+     cout << "Nilai akhir x = " << x << endl;
+     cout << "Jumlah perulangan = " << perulanganWhile << endl;
+     cout << endl;
+     cout << " Perulangan dengan do while" << endl;
+     
 }
