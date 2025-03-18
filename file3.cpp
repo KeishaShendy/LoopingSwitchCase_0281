27f49630229146565f7f38f7a15ee725f3d5fca0
keishashendy <shendyarrafli7@gmail.com>
2025-03-18 14:45:36 +0700

    Menambahkan case 1,2,dan 3

--- file3.cpp
+++ file3.cpp
@@ -16,7 +16,40 @@ int main(){
         cout << "<===============Menu=============>" << endl;
         cout << "1. Menghitung luas persegi panjang" << endl;
         cout << "2. Menghitung luas segitiga" << endl;
-        cout << "3. Exit" endll;
+        cout << "3. Exit" << endl;
         cout << pilihan;
-    }
+        switch(pilihan){
+            case 1:
+            cout << endl;
+            cout << "Menghitung luas persegi panjang" << endl;
+            cout << endl;
+            cout << "Masukkan panjang = ";
+            cin >> panjang;
+            cout << "Masukankan lebar = ";
+            cin >> lebar;
+            cout << "Luas persegi panjang = " <<
+            hitungLuasPersegiPanjang(panjang, lebar) << endl;
+            cout << endl;
+            break;
+        case 2:
+        cout << endl;
+        cout << "Menghitung luas segitiga" << endl;
+        cout << endl;
+        cout << "Masukkan alas = ";
+        cin >> alas;
+        cout << "Masukankan tinggi = ";
+        cin >> tinggi;
+        cout << "Luas segitiga = " <<
+        hitungLuasSegitiga(alas, tinggi) << endl;
+        cout << endl;
+        break;
+        case 3:
+        break;
+        default :
+        cout << endl;
+        cout << "ilihan anda salah!" << endl;
+        cout << endl;
+        break;
+        }
+    }while(pilihan != 3);
 }
